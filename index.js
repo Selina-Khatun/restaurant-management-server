6f9d5bece113e3696eeecc29f432ebb47cdf552b
Md Tasdid Nayem <selinakhatun982520@gmail.com>
2023-11-08 04:48:01 +0600

    created foodCollection

--- index.js
+++ index.js
@@ -28,6 +28,13 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const foodCollection=client.db('restaurantManagement').collection('foods')
+        app.get('/foods',async(req,res)=>{
+            const cursor=foodCollection.find();
+            const result=await cursor.toArray();
+            res.send(result);
+        })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
